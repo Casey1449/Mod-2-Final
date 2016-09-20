223c144f8875b670253cad06408749bee46a2933
Casey Metz <caseymetz@gmail.com>
2016-09-19 18:03:25 -0600

    refactored functions into helper files

--- test/test-index.js
+++ test/test-index.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert;
-const scoreWord = require('../lib/components/score-word.js');
-const scoreLetter = require('../lib/components/score-word.js');
+import scoreLetter from '../lib/helpers/score-letter.js';
+import scoreWord from '../lib/helpers/score-word.js';
 
 describe('the test bundle', function () {
   it('should be wired up', function () {
@@ -8,25 +8,20 @@ describe('the test bundle', function () {
   });
 });
 
-describe('the scoreLetter function', function (){
+describe('the scoreLetter function', function () {
   it('should return the number value of a capital letter', function () {
     let hi = scoreLetter('K');
     assert.equal(hi, 5);
   });
-
-  // it('should not be case sensitive', function () {
-  //   let hi = scoreLetter('k');
-  //   assert.equal(hi, 5);
-  // });
 });
 
-describe('the scoreWord function', function (){
+describe('the scoreWord function', function () {
   it('should return the number value of a word', function () {
     let value = scoreWord('hello');
     assert.equal(value, 8);
   });
 
-  it ('should not be case sensitive', function (){
+  it ('should not be case sensitive', function () {
     let val1 = scoreWord('hello');
     let val2 = scoreWord('HElLo');
     assert.equal(val1, val2);
@@ -46,24 +41,8 @@ describe('the scoreWord function', function (){
     assert.equal(hi, 5);
   });
 
-  it('accept a second argument that multiplies the returned result', function () {
+  it('should accept a second argument that multiplies the returned result', function () {
     let value = scoreWord('hello', 2);
     assert.equal(value, 16);
   });
 });
-
-// function scoreLetter(a) {
-//   let num = null;
-//   _.forOwn(letterScores, function(value, key) {if (a === key) {num =value;}
-//   });
-//   return num;
-// }
-//
-// function scoreWord(word) {
-//   word = word.toUpperCase();
-//   let val = null;
-//   for (var i = 0; i < word.length; i++){
-//    val = val + scoreLetter(word.charAt(i));
-//   }
-//   return val;
-// }
